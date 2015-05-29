6d27b700cc4e518c53704623cbce520df657cdcd
Tyler Ellis <tyler.ellis21@gmail.com>
2015-05-29 04:46:16 -0700

    Implemented the ray to plane intersection method.

--- include/nex/math/ray.h
+++ include/nex/math/ray.h
@@ -2,6 +2,7 @@
 #define RAY_H_INCLUDE
 
 #include <nex/math/vec3.h>
+#include <nex/math/plane.h>
 
 namespace nx
 {
@@ -15,6 +16,13 @@ namespace nx
          * @param direction = unit vector direction.
          */
         Ray(vec3f position, vec3f direction);
+
+        /**
+         * @brief Determines whether this Ray intersects a specified Plane.
+         * @param The Plane with which to calculate this Ray's intersection.
+         * @return intersection result.
+         */
+        float intersects(const Plane& plane) const;
 
         /**
          * @brief Specifies the starting point of the Ray.
--- src/nex/math/ray.cpp
+++ src/nex/math/ray.cpp
@@ -1,4 +1,5 @@
 #include <nex/math/ray.h>
+#include <cmath>
 
 namespace nx
 {
@@ -7,5 +8,29 @@ Ray::Ray(vec3f position, vec3f direction) :
     position(position),
     direction(direction)
 { }
+
+float Ray::intersects(const Plane& plane) const
+{
+    const float dotA = (plane.normal.x * direction.x + plane.normal.y * direction.y + plane.normal.z * direction.z);
+
+    //Check for out of bounds.
+    if ((double)abs(dotA) < 9.99999974737875E-06)
+        return 0.0f;
+
+    const float dotB = (plane.normal.x * position.x + plane.normal.y * position.y + plane.normal.z * position.z);
+
+    float distance = (-plane.distance - dotB) / dotA;
+
+    //Check for out of bounds.
+    if (distance < 0.0f)
+    {
+        if ((double)distance < -9.99999974737875E-06)
+            return 0.0f;
+
+        distance = 0.0f;
+    }
+
+    return distance;
+}
 
 } //namespace nx
