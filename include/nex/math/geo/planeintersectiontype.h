9549e220fb44baf2d9fe9180fd8f0397ba6b8054
Tyler Ellis <tyler.ellis21@gmail.com>
2015-05-29 04:14:50 -0700

    Implemented the plane intersection type enum.

--- include/nex/math/geo/planeintersectiontype.h
+++ include/nex/math/geo/planeintersectiontype.h
@@ -1,1 +1,16 @@
-//TODO (Tyler): Implement this.
+#ifndef PLANEINTERSECIONTYPE_H_INCLUDE
+#define PLANEINTERSECIONTYPE_H_INCLUDE
+
+namespace nx
+{
+
+enum PlaneIntersectionType
+{
+    Intersecting = 0,
+    Front = 1,
+    Back = -1
+};
+
+} //namespace nx
+
+#endif // PLANEINTERSECIONTYPE_H_INCLUDE
