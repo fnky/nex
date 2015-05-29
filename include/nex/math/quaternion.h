bb8fedac98ea28aa4ee5fd54f9ec8b12d93e333a
Tyler Ellis <tyler.ellis21@gmail.com>
2015-05-29 01:04:27 -0700

    Implemented the base class for the Quaternion.

--- include/nex/math/quaternion.h
+++ include/nex/math/quaternion.h
@@ -1,1 +1,38 @@
-//TODO (Tyler): Implement this.
+#ifndef QUATERNION_H_INCLUDE
+#define QUATERNION_H_INCLUDE
+
+namespace nx
+{
+    template <typename T>
+    class Quaternion
+    {
+    public:
+
+        /**
+         * @brief Construct an identity matrix.
+         */
+        Quaternion();
+
+        /**
+         * @brief Construct a quaternion with the specified values.
+         * @param x = x value.
+         * @param y = y value.
+         * @param z = z value.
+         * @param w = w value.
+         */
+        Quaternion(const T x, const T y, const T z, const T w);
+
+        T x;
+        T y;
+        T z;
+        T w;
+    };
+
+#include <nex/math/quaternion.inl>
+
+    typedef Quaternion<float> quatf;
+    typedef Quaternion<double> quatd;
+
+} // namespace nx
+
+#endif // QUATERNION_H_INCLUDE
--- include/nex/math/quaternion.inl
+++ include/nex/math/quaternion.inl
@@ -1,1 +1,17 @@
-//TODO (Tyler): Implement this.
+#include <cmath>
+
+template <typename T>
+inline Quaternion::Quaternion() :
+    x(static_cast<T>(0.0)),
+    y(static_cast<T>(0.0)),
+    z(static_cast<T>(0.0)),
+    w(static_cast<T>(1.0))
+{ }
+
+template <typename T>
+inline Quaternion::Quaternion(const T x, const T y, const T z, const T w) :
+    x(x),
+    y(y),
+    z(z),
+    w(w)
+{ }
