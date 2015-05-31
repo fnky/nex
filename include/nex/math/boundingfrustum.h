e9bace577196447c324d5629f87cc0fc086ec0d4
Tyler Ellis <tyler.ellis21@gmail.com>
2015-05-31 04:24:14 -0700

    Implemented a large part of the bounding frustum class.

--- include/nex/math/boundingfrustum.h
+++ include/nex/math/boundingfrustum.h
@@ -1,12 +1,139 @@
 #ifndef BOUNDINGFRUSTUM_H_INCLUDE
 #define BOUNDINGFRUSTUM_H_INCLUDE
 
+#include <nex/system/typedefs.h>
+#include <nex/math/vec3.h>
+#include <nex/math/matrix.h>
+#include <nex/math/gjk.h>
+#include <nex/math/plane.h>
+#include <nex/math/boundingbox.h>
+
 namespace nx
 {
     class BoundingFrustum
     {
     public:
+
+        BoundingFrustum();
+
+        /**
+         * @brief Specifies the total number of corners (8) in the BoundingFrustum.
+         */
+        const int CornerCount = 8;
+
+        /**
+         * @brief Bounding Frustum Plane Index
+         */
+        enum BFPlaneIndex {
+            NearPlaneIndex = 0,
+            FarPlaneIndex = 1,
+            LeftPlaneIndex = 2,
+            RightPlaneIndex = 3,
+            TopPlaneIndex = 4,
+            BottomPlaneIndex = 5
+        };
+
+        /**
+         * @brief Get the specified plane
+         * @param index = index of the plane to get.
+         * @return the specified plane.
+         */
+        const Plane getPlane(BFPlaneIndex index) const {
+            return mPlanes[index];
+        }
+
+        /**
+         * @brief Get a pointer to the current corner array.
+         * @return const pointer.
+         */
+        const vec3f* getCorners() const { return mCornerArray; }
+
+        /**
+         * @brief Get the current matrix.
+         * @return the matrix.
+         */
+        Matrix getMatrix() const { return mMatrix; }
+
+        /**
+         * @brief Checks whether the current BoundingFrustum intersects a BoundingBox.
+         * @param box = The BoundingBox to check for intersection with.
+         * @return true if the BoundingFrustum and BoundingBox intersect; false otherwise.
+         */
+        bool intersects(const BoundingBox& box);
+
+        /**
+         * @brief Checks whether the current BoundingFrustum intersects the specified BoundingFrustum.
+         * @param frustum = The BoundingFrustum to check for intersection.
+         * @return the intersection result.
+         */
+        bool intersects(BoundingFrustum& frustum);
+
+        /**
+         * @brief Checks whether the current BoundingFrustum intersects a Ray.
+         * @param ray = The Ray to check for intersection with.
+         * @return Distance at which the ray intersects the BoundingFrustum or 0 if there is no intersection.
+         */
+        float intersects(const Ray& ray);
+
+        /**
+         * @brief Checks whether the current BoundingFrustum contains the specified point.
+         * @param point = The point to check against the current BoundingFrustum.
+         * @return the containment type.
+         */
+        ContainmentType contains(const vec3f& point) const;
+
+        /**
+         * @brief Checks whether the current BoundingFrustum contains the specified BoundingBox.
+         * @param box = The BoundingBox to check against the current BoundingFrustum.
+         * @return the containment type.
+         */
+        ContainmentType contains(const BoundingBox& box) const;
+
+        /**
+         * @brief Checks whether the current BoundingFrustum contains the specified BoundingFrustum.
+         * @param frustum = The BoundingFrustum to check against the current BoundingFrustum.
+         * @return the containment type.
+         */
+        ContainmentType contains(BoundingFrustum frustum);
+
+        /**
+         * @brief Checks whether the current BoundingFrustum contains the specified BoundingSphere.
+         * @param sphere = The BoundingSphere to check against the current BoundingFrustum.
+         * @return the containment type.
+         */
+        ContainmentType contains(const BoundingSphere& sphere) const;
+
+        /**
+         * @brief Checks whether the current BoundingFrustum intersects the specified Plane.
+         * @param plane = The Plane to check for intersection.
+         * @return the itnersection type.
+         */
+        PlaneIntersectionType intersects(const Plane& plane) const;
+
+        /**
+         * @brief Compute the bounding frustum from the given matrix.
+         * @param matrix = matrix to compute from.
+         */
+        void setMatrix(const Matrix& matrix);
+
+        /**
+         * @brief supportMapping
+         * @param v
+         * @return
+         */
+        vec3f supportMapping(const vec3f v);
+
     private:
+
+        const int NumPlanes = 6;
+
+        vec3f mCornerArray[8];
+
+        Plane mPlanes[6];
+
+        Matrix mMatrix;
+        GJK mGJK;
+
     };
 } //namespace nx
 
---
+++ src/nex/math/boundingfrustum.cpp
@@ -0,0 +1,312 @@
+#include <nex/math/boundingfrustum.h>
+#include <nex/math/plane.h>
+#include <nex/math/ray.h>
+
+#include <limits>
+
+namespace nx
+{
+    BoundingFrustum::BoundingFrustum()
+    { }
+
+    void BoundingFrustum::setMatrix(const Matrix& value)
+    {
+        //Store our matrix.
+        mMatrix = value;
+
+        //Calculate all of the planes for the bounding frustum.
+
+        mPlanes[2].normal.x = -value[0][3] - value[0][0];
+        mPlanes[2].normal.y = -value[1][3] - value[1][0];
+        mPlanes[2].normal.z = -value[2][3] - value[2][0];
+        mPlanes[2].distance = -value[3][3] - value[3][0];
+
+        mPlanes[3].normal.x = -value[0][3] + value[0][0];
+        mPlanes[3].normal.y = -value[1][3] + value[1][0];
+        mPlanes[3].normal.z = -value[2][3] + value[2][0];
+        mPlanes[3].distance = -value[3][3] + value[3][0];
+
+        mPlanes[4].normal.x = -value[0][3] + value[0][1];
+        mPlanes[4].normal.y = -value[1][3] + value[1][1];
+        mPlanes[4].normal.z = -value[2][3] + value[2][1];
+        mPlanes[4].distance = -value[3][3] + value[3][1];
+
+        mPlanes[5].normal.x = -value[0][3] - value[0][1];
+        mPlanes[5].normal.y = -value[1][3] - value[1][1];
+        mPlanes[5].normal.z = -value[2][3] - value[2][1];
+        mPlanes[5].distance = -value[3][3] - value[3][1];
+
+        mPlanes[0].normal.x = -value[0][2];
+        mPlanes[0].normal.y = -value[1][2];
+        mPlanes[0].normal.z = -value[2][2];
+        mPlanes[0].distance = -value[3][2];
+
+        mPlanes[1].normal.x = -value[0][3] + value[0][2];
+        mPlanes[1].normal.y = -value[1][3] + value[1][2];
+        mPlanes[1].normal.z = -value[2][3] + value[2][2];
+        mPlanes[1].distance = -value[3][3] + value[3][2];
+
+        //Normalize all of the planes.
+        for (int index = 0; index < 6; ++index)
+        {
+            const float oneOverLength = 1.0f / mPlanes[index].normal.length();
+
+            mPlanes[index].normal *= oneOverLength;
+            mPlanes[index].distance *= oneOverLength;
+        }
+
+        Ray intersectionLine1 = Plane::computeIntersectionLine(mPlanes[0], mPlanes[2]);
+        mCornerArray[0] = intersectionLine1.computeIntersection(mPlanes[4]);
+        mCornerArray[3] = intersectionLine1.computeIntersection(mPlanes[5]);
+
+        Ray intersectionLine2 = Plane::computeIntersectionLine(mPlanes[3], mPlanes[0]);
+        mCornerArray[1] = intersectionLine2.computeIntersection(mPlanes[4]);
+        mCornerArray[2] = intersectionLine2.computeIntersection(mPlanes[5]);
+
+        intersectionLine2 = Plane::computeIntersectionLine(mPlanes[2], mPlanes[1]);
+        mCornerArray[4] = intersectionLine2.computeIntersection(mPlanes[4]);
+        mCornerArray[7] = intersectionLine2.computeIntersection(mPlanes[5]);
+
+        intersectionLine2 = Plane::computeIntersectionLine(mPlanes[1], mPlanes[3]);
+        mCornerArray[5] = intersectionLine2.computeIntersection(mPlanes[4]);
+        mCornerArray[6] = intersectionLine2.computeIntersection(mPlanes[5]);
+    }
+
+    vec3f BoundingFrustum::supportMapping(const vec3f v)
+    {
+        int searchIndex = 0;
+        float thetaA = vec3f::dot(mCornerArray[0], v);
+
+        for (int index2 = 1; index2 < 8; ++index2)
+        {
+            float thetaB = vec3f::dot(mCornerArray[index2], v);
+            if (thetaB > thetaA)
+            {
+                searchIndex = index2;
+                thetaA = thetaB;
+            }
+        }
+
+        return mCornerArray[searchIndex];
+    }
+
+    bool BoundingFrustum::intersects(const BoundingBox& box)
+    {
+        mGJK.reset();
+
+        vec3f result1 = mCornerArray[0] - box.min;
+
+        if ((double)result1.lengthSquared() < 9.99999974737875E-06)
+            result1 = mCornerArray[0] - box.max;
+
+        float num1 = std::numeric_limits<float>::max();
+        float num2;
+        do
+        {
+            vec3f v;
+
+            v.x = -result1.x;
+            v.y = -result1.y;
+            v.z = -result1.z;
+
+            vec3f result2 = supportMapping(v);
+            vec3f result3 = supportMapping(result1);
+            vec3f result4 = result2 - result3;
+
+            if (result1.x * result4.x + result1.y * result4.y + result1.z * result4.z > 0.0f)
+                return false;
+
+            mGJK.addSupportPoint(result4);
+
+            result1 = mGJK.closestPoint();
+            float num3 = num1;
+            num1 = result1.lengthSquared();
+            if ((double)num3 - (double)num1 <= 9.99999974737875E-06 * (double)num3)
+                return false;
+            num2 = 4E-05f * mGJK.maxLengthSquared();
+        }
+        while (!mGJK.fullSimplex() && num1 >= num2);
+    }
+
+    bool BoundingFrustum::intersects(BoundingFrustum& frustum)
+    {
+        mGJK.reset();
+
+        const vec3f* otherCorners = frustum.getCorners();
+
+        vec3f result1 = mCornerArray[0] - otherCorners[0];
+
+        if ((double)result1.lengthSquared() < 9.99999974737875E-06)
+            result1 = mCornerArray[0] - otherCorners[1];
+
+        float num1 = std::numeric_limits<float>::max();
+        float num2;
+        do
+        {
+            vec3f v;
+
+            v.x = -result1.x;
+            v.y = -result1.y;
+            v.z = -result1.z;
+
+            vec3f result2 = supportMapping(v);
+            vec3f result3 = frustum.supportMapping(result1);
+            vec3f result4 = result2 - result3;
+
+            if (result1.x * result4.x + result1.y * result4.y + result1.z * result4.z > 0.0f)
+                return false;
+
+            mGJK.addSupportPoint(result4);
+
+            result1 = mGJK.closestPoint();
+
+            float num3 = num1;
+            num1 = result1.lengthSquared();
+            num2 = 4E-05f * mGJK.maxLengthSquared();
+            if ((double) num3 - (double)num1 <= 9.99999974737875E-06 * (double) num3)
+                return false;
+        }
+        while (!mGJK.fullSimplex() && num1 >= num2);
+        return true;
+    }
+
+    float BoundingFrustum::intersects(const Ray& ray)
+    {
+        ContainmentType result1 = contains(ray.position);
+        if (result1 == ContainmentType::Contains)
+        {
+            return  0.0f;
+        }
+        else
+        {
+            float minDistance = std::numeric_limits<float>::min();//float.MinValue;
+            float maxDistance = std::numeric_limits<float>::max();//float.MaxValue;
+
+            for (auto plane : mPlanes)
+            {
+                float dotA = vec3f::dot(ray.direction, plane.normal);
+                float dotB = vec3f::dot(ray.position, plane.normal);
+
+                dotB += plane.distance;
+
+                if (abs(dotA) < 9.99999974737875E-06)
+                {
+                    if (dotB > 0.0f)
+                        return 0.0f;
+                }
+                else
+                {
+                    float num3 = -dotB / dotA;
+                    if (dotA < 0.0f)
+                    {
+                        if (num3 > maxDistance)
+                            return 0.0f;
+
+                        if (num3 > minDistance)
+                            minDistance = num3;
+                    }
+                    else
+                    {
+                        if (num3 < minDistance)
+                            return 0.0f;
+                        if (num3 < maxDistance)
+                            maxDistance = num3;
+                    }
+                }
+            }
+
+            float clampedResult = minDistance >= 0.0f ? minDistance : maxDistance;
+
+            if (clampedResult < 0.0f)
+                return 0.0f;
+
+            return clampedResult;
+        }
+    }
+
+    PlaneIntersectionType BoundingFrustum::intersects(const Plane& plane) const
+    {
+        int result = 0;
+        for (int index = 0; index < 8; ++index)
+        {
+            float dotResult = vec3f::dot(mCornerArray[index], plane.normal);
+
+            if (dotResult + plane.distance > 0.0f)
+                result |= 1;
+            else
+                result |= 2;
+
+            if (result == 3)
+                return PlaneIntersectionType::Intersecting;
+        }
+        return result != 1 ? PlaneIntersectionType::Back : PlaneIntersectionType::Front;
+    }
+
+    ContainmentType BoundingFrustum::contains(const vec3f& point) const
+    {
+        for (auto plane : mPlanes)
+        {
+            if (((plane.normal.x * point.x + plane.normal.y * point.y + plane.normal.z * point.z) + plane.distance) > 9.99999974737875E-06f)
+                return ContainmentType::Disjoint;
+        }
+        return ContainmentType::Contains;
+    }
+
+    ContainmentType BoundingFrustum::contains(const BoundingBox& box) const
+    {
+        bool flag = false;
+        for (auto plane : mPlanes)
+        {
+            switch (plane.intersects(box))
+            {
+            case PlaneIntersectionType::Front:
+                return ContainmentType::Disjoint;
+            case PlaneIntersectionType::Intersecting:
+                flag = true;
+                break;
+            }
+        }
+        return !flag ? ContainmentType::Contains : ContainmentType::Intersects;
+    }
+
+    ContainmentType BoundingFrustum::contains(BoundingFrustum frustum)
+    {
+        ContainmentType containmentType = ContainmentType::Disjoint;
+        if (intersects(frustum))
+        {
+            containmentType = ContainmentType::Contains;
+            const vec3f* cornerPtr = frustum.getCorners();
+            for (int index = 0; index < 8; ++index)
+            {
+                if (contains(cornerPtr[index]) == ContainmentType::Disjoint)
+                {
+                    containmentType = ContainmentType::Intersects;
+                    break;
+                }
+            }
+        }
+        return containmentType;
+    }
+
+    ContainmentType BoundingFrustum::contains(const BoundingSphere& sphere) const
+    {
+        const vec3f center = sphere.center;
+        const float radius = sphere.radius;
+
+        int result = 0;
+
+        for (auto plane : mPlanes)
+        {
+            float cross = (plane.normal.x * center.x + plane.normal.y * center.y + plane.normal.z * center.z) + plane.distance;
+
+            if (cross > radius)
+                return ContainmentType::Disjoint;
+
+            if (cross < -radius)
+                ++result;
+        }
+
+        return result != 6 ? ContainmentType::Intersects : ContainmentType::Contains;
+    }
+
+} //namespace nx
