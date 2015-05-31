998a337a4a9183e6551a0c80ee2a8e37bc3def27
Tyler Ellis <tyler.ellis21@gmail.com>
2015-05-31 01:42:31 -0700

    Added a blank bounding sphere. Implemented a merge method for the bounding sphere class.

--- include/nex/math/boundingsphere.h
+++ include/nex/math/boundingsphere.h
@@ -1,6 +1,7 @@
 #ifndef BOUNDINGSPHERE_H_INCLUDE
 #define BOUNDINGSPHERE_H_INCLUDE
 
+#include <nex/math/mathhelper.h>
 #include <nex/math/vec3.h>
 
 namespace nx
@@ -14,10 +15,23 @@ namespace nx
 
         /**
          * @brief Creates a new instance of BoundingSphere.
+         */
+        BoundingSphere();
+
+        /**
+         * @brief Creates a new instance of BoundingSphere.
          * @param center = Center point of the sphere.
          * @param radius = Radius of the sphere.
          */
         BoundingSphere(const vec3f center, const float radius);
+
+        /**
+         * @brief Creates a BoundingSphere that contains the two specified BoundingSphere instances.
+         * @param original = BoundingSphere to be merged.
+         * @param additional = BoundingSphere to be merged.
+         * @return the resulting sphere.
+         */
+        static BoundingSphere createMerged(const BoundingSphere& original, const BoundingSphere& additional);
 
         /**
          * @brief The center point of the sphere.
--- src/nex/math/boundingsphere.cpp
+++ src/nex/math/boundingsphere.cpp
@@ -3,9 +3,41 @@
 namespace nx
 {
 
+BoundingSphere::BoundingSphere() :
+    center(vec3f()),
+    radius(0.0f)
+{ }
+
 BoundingSphere::BoundingSphere(const vec3f center, const float radius) :
     center(center),
     radius(radius)
 { }
 
+inline BoundingSphere BoundingSphere::createMerged(const BoundingSphere& original, const BoundingSphere& additional)
+{
+    const vec3f result = additional.center - original.center;
+    const float resultLength = result.length();
+    const float radiusA = original.radius;
+    const float radiusB = additional.radius;
+
+    if (radiusA + radiusB >= resultLength)
+    {
+        if (radiusA - radiusB >= resultLength)
+            return original;
+        if (radiusB - radiusA >= resultLength)
+            return additional;
+    }
+
+    const vec3f oneOverLength = result * (1.0f / resultLength);
+
+    const float minRad = min(-radiusA, resultLength - radiusB);
+    const float maxRad = ((max(radiusA, resultLength + radiusB) - minRad) * 0.5f);
+
+    BoundingSphere resultSphere;
+
+    resultSphere.center = original.center + oneOverLength * (maxRad + minRad);
+    resultSphere.radius = maxRad;
+
+    return resultSphere;
+}
 } //namespace nx
