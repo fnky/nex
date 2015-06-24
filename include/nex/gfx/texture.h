a4c9388cdfddab4d9702d29042ef9e0b863de41e
Tyler Ellis <tyler.ellis21@gmail.com>
2015-06-23 19:12:49 -0700

    Updated the texture class to now support smoothing and uv wrapping.

--- include/nex/gfx/texture.h
+++ include/nex/gfx/texture.h
@@ -3,6 +3,8 @@
 
 // Nex includes.
 #include <nex/system/typedefs.h>
+#include <nex/math/vec2.h>
+#include <nex/gfx/image.h>
 
 #include <GL/glew.h>
 
@@ -19,6 +21,14 @@ public:
     ~Texture();
 
     /**
+     * @brief Create the texture.
+     * @param width = Width of the texture.
+     * @param height = Height of the texture.
+     * @return true if creation was successful.
+     */
+    bool create(uint32 width, uint32 height);
+
+    /**
      * @brief Load a texture into the gpu texture from the specified file.
      * @param file = image file to load.
      * @return true if sucessfull.
@@ -26,16 +36,12 @@ public:
     bool loadFromFile(const std::string& file);
 
     /**
-     * @brief Get the pixel width of the image that was loaded into opengl.
-     * @return pixel width of the image.
+     * @brief Load the texture from an image.
+     * @param image = Image to load into the texture.
+     * @param area = Area of the image to load.
+     * @return true if loading was successful.
      */
-    uint32 getPixelWidth() const { return mPixelWidth; }
-
-    /**
-     * @brief Get the pixel height of the image that was loaded into opengl.
-     * @return pixel height of the image.
-     */
-    uint32 getPixelHeight() const { return mPixelHeight; }
+    bool loadFromImage(const Image& image, const recti& area = recti());
 
     /**
      * @brief Bind the texture to the opengl context.
@@ -43,19 +49,91 @@ public:
     inline void bind() const { glBindTexture(GL_TEXTURE_2D, mId); }
 
     /**
-     * @brief update
-     * @param pixels
-     * @param width
-     * @param height
-     * @param x
-     * @param y
+     * @brief Unbind the texture from the opengl context.
      */
-    void update(const uint8* pixels, unsigned int width, unsigned int height, unsigned int x, unsigned int y);
+    inline void unbind() const { glBindTexture(GL_TEXTURE_2D, 0); }
+
+    /**
+     * @brief Copy this texture to an image.
+     * @return The current image.
+     */
+    Image copyToImage() const;
+
+    /**
+     * @brief Update a part of the texture from an array of pixels.
+     * @param pixels = Array of pixels to copy to the texture.
+     * @param width = Width of the pixel region contained in pixels.
+     * @param height = Height of the pixel region contained in pixels.
+     * @param x = X offset in the texture where to copy the source pixels.
+     * @param y = Y offset in the texture where to copy the source pixels.
+     */
+    void update(const uint8* pixels, uint32 width, uint32 height, uint32 x, uint32 y);
+
+    /**
+     * @brief Update the whole texture from an array of pixels.
+     * @param pixels = Array of pixels to copy to the texture.
+     */
+    void update(const uint8* pixels);
+
+    /**
+     * @brief Update the texture from an image.
+     * @param image = Image to copy to the texture.
+     */
+    void update(const Image& image);
+
+    /**
+     * @brief Update a part of the texture from an image.
+     * @param image = Image to copy to the texture.
+     * @param x = X offset in the texture where to copy the source image.
+     * @param y = Y offset in the texture where to copy the source image.
+     */
+    void update(const Image& image, uint32 x, uint32 y);
+
+    /**
+     * @brief Query the maximum opengl texture size.
+     * @return The maximum texture size.
+     */
+    static uint32 getMaximumSize();
+
+    /**
+     * @brief Get the size of the image.
+     * @return The current image size.
+     */
+    inline vec2u size() const { return mSize; }
+
+    /**
+     * @brief Enable texture smoothing.
+     * @param smooth = True oder false.
+     */
+    void setSmooth(bool smooth);
+
+    /**
+     * @brief Get the state of the this textures smoothing.
+     * @return the state of the smooth.
+     */
+    inline bool getSmooth() const { return mSmooth; }
+
+    /**
+     * @brief Set the texture to repeate uv coordinates outside of the 0.0 <-> 0.1 range.
+     * @param repeat = Repeat or not.
+     */
+    void setRepeat(bool repeat);
+
+    /**
+     * @brief Get the state of the textures repeat.
+     * @return the repeat state.
+     */
+    inline bool getRepeat() const { return mRepeat; }
 
 private:
+    mutable bool mPixelsFlipped;
+
+    bool mSmooth;
+    bool mRepeat;
+
     uint32 mId;
-    uint32 mPixelWidth;
-    uint32 mPixelHeight;
+    vec2u mSize;
+    vec2u mActualSize;
 
     static int mTexturesAlive;
 };
--- src/nex/gfx/texture.cpp
+++ src/nex/gfx/texture.cpp
@@ -9,8 +9,18 @@ namespace nx
 
 int Texture::mTexturesAlive = 0;
 
+uint32 Texture::getMaximumSize()
+{
+    GLint size;
+    glGetIntegerv(GL_MAX_TEXTURE_SIZE, &size);
+
+    return static_cast<unsigned int>(size);
+}
+
 Texture::Texture() :
-    mId(0)
+    mId(0),
+    mPixelsFlipped(false),
+    mSmooth(false)
 {
     std::cout << "texture ctor" << std::endl;
 }
@@ -23,6 +33,64 @@ Texture::~Texture()
         glDeleteTextures(1, &mId);
         mId = 0;
     }
+}
+
+uint32 getValidSize(unsigned int size)
+{
+    // If hardware doesn't support NPOT textures, we calculate the nearest power of two
+    unsigned int powerOfTwo = 1;
+    while (powerOfTwo < size)
+        powerOfTwo *= 2;
+
+    return powerOfTwo;
+
+}
+
+bool Texture::create(uint32 width, uint32 height)
+{
+    // Check if texture parameters are valid before creating it
+    if ((width == 0) || (height == 0))
+    {
+        return false;
+    }
+
+    // Compute the internal texture dimensions depending on NPOT textures support
+    vec2u actualSize(getValidSize(width), getValidSize(height));
+
+    // Check the maximum texture size
+    uint32 maxSize = getMaximumSize();
+    if ((actualSize.x > maxSize) || (actualSize.y > maxSize))
+    {
+        std::cout << "Failed to create texture, its internal size is too high "
+              << "(" << actualSize.x << "x" << actualSize.y << ", "
+              << "maximum is " << maxSize << "x" << maxSize << ")"
+              << std::endl;
+        return false;
+    }
+
+    // All the validity checks passed, we can store the new texture settings
+    mSize.x = width;
+    mSize.y = height;
+    mActualSize = actualSize;
+    mPixelsFlipped = false;
+
+    // Create the OpenGL texture if it doesn't exist yet
+    if (!mId)
+    {
+        GLuint texture;
+        glGenTextures(1, &mId);
+        mId = static_cast<unsigned int>(texture);
+    }
+
+    // Initialize the texture
+    glBindTexture(GL_TEXTURE_2D, mId);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mActualSize.x, mActualSize.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, mRepeat ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, mRepeat ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mSmooth ? GL_LINEAR : GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, mSmooth ? GL_LINEAR : GL_NEAREST);
 }
 
 bool Texture::loadFromFile(const std::string& file)
@@ -56,22 +124,184 @@ bool Texture::loadFromFile(const std::string& file)
                 GL_RGBA, GL_UNSIGNED_BYTE, image.getPixelsPtr());
 
     // Setup our texture interpolation settings.
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mSmooth ? GL_LINEAR : GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, mSmooth ? GL_LINEAR : GL_NEAREST);
 
     // Specify what we do when we use a texture coordinate outside of the normal range (0.0, 0.1)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, mRepeat ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, mRepeat ? GL_CLAMP_TO_EDGE : GL_REPEAT);
 
     return true;
 }
 
-void Texture::update(const uint8* pixels, unsigned int width, unsigned int height, unsigned int x, unsigned int y)
+bool Texture::loadFromImage(const Image& image, const recti& area)
+{
+    // Retrieve the image size
+    int width = static_cast<int>(image.size().x);
+    int height = static_cast<int>(image.size().y);
+
+    // Load the entire image if the source area is either empty or contains the whole image
+    if (area.width == 0 || (area.height == 0) ||
+       ((area.x <= 0) && (area.y <= 0) && (area.width >= width) && (area.height >= height)))
+    {
+        // Load the entire image
+        if (create(image.size().x, image.size().y))
+        {
+            update(image);
+
+            // Force an OpenGL flush, so that the texture will appear updated
+            // in all contexts immediately (solves problems in multi-threaded apps)
+            glFlush();
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        // Load a sub-area of the image
+
+        // Adjust the rectangle to the size of the image
+        recti rectangle = area;
+        if (rectangle.x < 0) rectangle.x = 0;
+        if (rectangle.y < 0) rectangle.y = 0;
+
+        if (rectangle.x + rectangle.width > width)
+            rectangle.width  = width - rectangle.x;
+
+        if (rectangle.y + rectangle.height > height)
+            rectangle.height = height - rectangle.y;
+
+        // Create the texture and upload the pixels
+        if (create(rectangle.width, rectangle.height))
+        {
+            // Copy the pixels to the texture, row by row
+            const uint8* pixels = image.getPixelsPtr() + 4 * (rectangle.x + (width * rectangle.y));
+            glBindTexture(GL_TEXTURE_2D, mId);
+            for (int i = 0; i < rectangle.height; ++i)
+            {
+                glTexSubImage2D(GL_TEXTURE_2D, 0, 0, i, rectangle.width, 1, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+                pixels += 4 * width;
+            }
+
+            // Force an OpenGL flush, so that the texture will appear updated
+            // in all contexts immediately (solves problems in multi-threaded apps)
+            glFlush();
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+void Texture::update(const uint8* pixels, uint32 width, uint32 height, uint32 x, uint32 y)
 {
     if (mId > 0) {
         glBindTexture(GL_TEXTURE_2D, mId);
         glTexSubImage2D(GL_TEXTURE_2D, 0, x, y, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
     }
+}
+
+void Texture::update(const uint8* pixels)
+{
+    // Update the whole texture
+    update(pixels, mSize.x, mSize.y, 0, 0);
+}
+
+void Texture::update(const Image& image)
+{
+    // Update the whole texture
+    update(image.getPixelsPtr(), image.size().x, image.size().y, 0, 0);
+}
+
+void Texture::update(const Image& image, uint32 x, uint32 y)
+{
+    update(image.getPixelsPtr(), image.size().x, image.size().y, x, y);
+}
+
+Image Texture::copyToImage() const
+{
+    // Easy case: empty texture
+    if (!mId)
+        return Image();
+
+    // Create an array of pixels
+    std::vector<uint8> pixels(mSize.x * mSize.y * 4);
+
+    if ((mSize == mActualSize) && !mPixelsFlipped)
+    {
+        // Texture is not padded nor flipped, we can use a direct copy
+        glBindTexture(GL_TEXTURE_2D, mId);
+        glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, &pixels[0]);
+    }
+    else
+    {
+        // Texture is either padded or flipped, we have to use a slower algorithm
+
+        // All the pixels will first be copied to a temporary array
+        std::vector<uint8> allPixels(mActualSize.x * mActualSize.y * 4);
+
+        glBindTexture(GL_TEXTURE_2D, mId);
+        glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, &allPixels[0]);
+
+        // Then we copy the useful pixels from the temporary array to the final one
+        const uint8* src = &allPixels[0];
+
+        uint8* dst = &pixels[0];
+
+        int srcPitch = mActualSize.x * 4;
+        int dstPitch = mSize.x * 4;
+
+        // Handle the case where source pixels are flipped vertically
+        if (mPixelsFlipped)
+        {
+            src += srcPitch * (mSize.y - 1);
+            srcPitch = -srcPitch;
+        }
+
+        for (unsigned int i = 0; i < mSize.y; ++i)
+        {
+            std::memcpy(dst, src, dstPitch);
+            src += srcPitch;
+            dst += dstPitch;
+        }
+    }
+
+
+    // Create the image
+    Image image;
+    image.create(mSize.x, mSize.y, &pixels[0]);
+
+    return image;
+}
+
+void Texture::setSmooth(bool smooth)
+{
+    mSmooth = smooth;
+
+    // Make sure we are modifying this texture.
+    bind();
+
+    // Setup our texture interpolation settings.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mSmooth ? GL_LINEAR : GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, mSmooth ? GL_LINEAR : GL_NEAREST);
+}
+
+void Texture::setRepeat(bool repeat)
+{
+    mRepeat = repeat;
+    // Make sure we are modifying this texture.
+    bind();
+
+    // Specify what we do when we use a texture coordinate outside of the normal range (0.0, 0.1)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, mRepeat ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, mRepeat ? GL_CLAMP_TO_EDGE : GL_REPEAT);
 }
 
 } //namespace nx
